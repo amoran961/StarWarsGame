3f9466e0ee35b24eaa5350af999fe4392af17b02
amoran961 <anthony_mm_96@hotmail.com>
2017-09-16 15:38:29 -0500

    Subida del juego
    Primer juego

---
+++ views/__init__.py
@@ -0,0 +1,25 @@
+import imageio
+imageio.plugins.ffmpeg.download()
+from moviepy.editor import *
+from moviepy.video.fx.resize import resize
+import pygame
+
+pygame.init()
+gameDisplay= pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+pygame.display.set_caption('Holi')
+clock= pygame.time.Clock()
+clip = VideoFileClip('D:\intro\intro.mp4')
+clip.preview()
+crashed= False
+
+while not crashed:
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            crashed =True
+        print(event)
+
+    pygame.display.update()
+    clock.tick(60)
+pygame.quit()
+quit()
