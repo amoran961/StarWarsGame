554520d12f9f930c05fdeb2c7c27da024ec709be
amoran96 <anthony_mm_96@hotmail.com>
2017-09-18 23:28:04 -0500

    Música y menú base

--- views/__init__.py
+++ views/__init__.py
@@ -6,21 +6,57 @@ import pygame
 
 pygame.init()
 gameDisplay= pygame.display.set_mode((0,0),pygame.FULLSCREEN)
-pygame.display.set_caption('Holi')
+pygame.display.set_caption('Star Wars')
 clock= pygame.time.Clock()
+
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+
 clip = VideoFileClip('intro.mp4')
 clip.preview()
+
 pygame.display.update()
-crashed= False
 
-while not crashed:
-    for event in pygame.event.get():
 
-        if event.type == pygame.QUIT:
-            crashed =True
-        print(event)
+def game_loop():
+    crashed= False
+    while not crashed:
+        for event in pygame.event.get():
 
-    pygame.display.update()
-    clock.tick(60)
+            if event.type == pygame.QUIT:
+                crashed =True
+            print(event)
+
+        pygame.display.update()
+        clock.tick(60)
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def game_intro():
+    intro = True
+
+    bg = pygame.image.load("menu.png")
+    file = "menu.mp3"
+    pygame.mixer.init()
+    pygame.mixer.music.load(file)
+    pygame.mixer.music.play()
+
+    while intro and pygame.mixer.music.get_busy():
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        gameDisplay.blit(bg, (0, 0))
+        pygame.display.update()
+        clock.tick(15)
+
+
+game_intro()
+game_loop()
 pygame.quit()
 quit()
